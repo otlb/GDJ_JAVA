ff2d7cb8332fe2730b7c6b33d9d10ff0d9f477d6
otlb <an95621@naver.com>
2023-12-05 14:51:02 +0900

    add

--- src/com/winter/study1/Sutdy4For.java
+++ src/com/winter/study1/Sutdy4For.java
@@ -1,13 +1,34 @@
 package com.winter.study1;
 
+import java.util.Scanner;
+
 public class Sutdy4For {
 	public static void main(String[]args) {
 		
-		System.out.println("Master");
-		System.out.println("연습하기");
-		System.out.println("연습하기2");
 		
+				
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("1번입력 단발 2번입력 점사");
+		int danbal = sc.nextInt();
+		String tang = "탕";
+
+		
+		if(danbal==1) {
+		for(int i=1; i<4; i++) {
+			for(int j=1; j<31; j++) {
+				System.out.println(i+"번째탄창 "+j+"발");
+				}
+			}
+		}
+		if(danbal==2) {
+			for(int i=1; i<4; i++) {
+				for(int j=3; j<31; j=j+3) {
+					System.out.println(i+"번째탄창 "+j+"발");
+					System.out.println(tang);
+					}
+				}
+			}
 		
 		
 		
