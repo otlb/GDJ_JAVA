a0a76282ffa471525111b211d487341ab731ecb1
otlb <an95621@naver.com>
2023-12-05 14:27:10 +0900

    dd

---
+++ src/com/winter/study1/Sutdy4For.java
@@ -0,0 +1,14 @@
+package com.winter.study1;
+
+public class Sutdy4For {
+	public static void main(String[]args) {
+		
+		System.out.println("Master");
+		System.out.println("연습하기");
+		
+		
+		
+		
+		
+	}
+}
