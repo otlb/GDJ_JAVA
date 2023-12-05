b5b1d7c1dc241d3a2b1c61ee803e4b3d53a6dd36
otlb <an95621@naver.com>
2023-12-05 11:41:31 +0900

    test

--- src/com/winter/study1/Study1For.java
+++ src/com/winter/study1/Study1For.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Study1For {
 	public static void main(String[]args) {
 		System.out.println("프로그램시작");
+		System.out.println("테스트");
 	
 		Scanner sc = new Scanner(System.in);
 		
