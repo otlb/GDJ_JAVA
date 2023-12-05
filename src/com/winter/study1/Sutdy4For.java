cc17b20c95b651682a65ab291d93a4bce78118e0
otlb <an95621@naver.com>
2023-12-05 15:40:30 +0900

    add

--- src/com/winter/study1/Sutdy4For.java
+++ src/com/winter/study1/Sutdy4For.java
@@ -13,39 +13,26 @@ public class Sutdy4For {
 		
 		System.out.println("1번입력 단발 2번입력 점사");
 		int danbal = sc.nextInt();
-		String tang = "탕";
+
 		
-		for(int i=1; i<4;i++){
-			if(danbal==1){
-				for( danbal=1; danbal<31; danbal=danbal+1) {
-					System.out.println(i+"번째탄창 "+danbal+"발");
-				}
-			}else if(danbal==2) {
-				for( danbal=3; danbal<31; danbal=danbal+1) {
-					System.out.println(i+"번째탄창 "+danbal+"발");
-			}
+		
+		
+		
+		
+	
 			
-		}			
-		
-	}
-		
-		
-		
-		
-		
-		
-		
 //		for(int i=1; i<4;i++){
 //			if(danbal==1) {
 //				for(int j=1; j<31; j++) {
 //				System.out.println(i+"번째탄창 "+j+"발");
-//				}				
-//			}else if(danbal==2) {
-//				for(int j=3; j<31; j=j+3) {
-//					System.out.println(tang);
-//					System.out.println(i+"번째탄창 "+j+"발");
-//					}				
+//				}		
+//				System.out.println("탄창교체");
 //			}
+//				for(int j=3; j<31; j=j+3) {
+//					System.out.println(i+"번째탄창 "+j+"발");
+//					}	
+//				System.out.println("탄창교체");
+//			
 //			
 //		}
 
