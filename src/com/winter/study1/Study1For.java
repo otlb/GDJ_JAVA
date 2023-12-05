3cad2c380e00f47ef3514397e4185fd79405573b
otlb <an95621@naver.com>
2023-12-05 11:21:20 +0900

    first

---
+++ src/com/winter/study1/Study1For.java
@@ -0,0 +1,23 @@
+package com.winter.study1;
+
+import java.util.Scanner;
+
+public class Study1For {
+	public static void main(String[]args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수 하나를 입력하세요");
+		int num = sc.nextInt();
+		
+		//입력받은 정수에 약수를 출력하세요
+		
+		for(int i=1; i<=num; i++ ) {
+			if(num%i==0) {
+				System.out.println(i);
+			}
+		}		
+		
+	}
+
+}
